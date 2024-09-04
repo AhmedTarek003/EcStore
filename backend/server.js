76b587dbcae524fcdc9fdf934591725aa43a413e
ahemd003 <at3616464@gmail.com>
2024-09-04 16:41:19 +0300

    kkk

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,14 @@ connectMongoDB();
 app.use(
   cors({
     origin: "http://localhost:3000",
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
-    ],
+    // methods: ["GET", "POST", "DELETE", "PUT"],
+    // allowedHeaders: [
+    //   "Content-Type",
+    //   "Authorization",
+    //   "Cache-Control",
+    //   "Expires",
+    //   "Pragma",
+    // ],
     credentials: true,
   })
 );
